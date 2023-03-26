c64658d4b675499673461f18f5ad6c6add176247
Abdalrhman Hamida <96482182+Abdalrhman7mida@users.noreply.github.com>
2023-03-27 01:53:30 +0200

    Optimize & enhance Bisection_algo
    - [x] Removing unnecessary print statements.
    - [x] Using string format.
    - [x] Handling inputs from user.
    - [x] Comments to explain.

--- algo/Bisection_Search.py
+++ algo/Bisection_Search.py
@@ -1,19 +1,29 @@
 print('think of a number between 0 to 100, and we will get it right fast !')
+# Set initial range of numbers
 low = 0
 high = 100
-medium = (low + high) / 2
+# Initialize state variable to control the loop
 state = True
+
+# Loop until the secrete number is found
 while state:
-    print('Is your number ' + str(medium))
-    guess = input('Enter h to indicate the guess is too high, l to indicate is too low, c to indicate it\'s correct: ')
+    # Calculate the midpoint of the range
+    medium = (high + low) // 2
+    # Ask the user if their secret number is the midpoint
+    guess = input(
+        f"Is your number {medium}? Enter 'h' to indicate the guess is too high, 'l' to indicate is too low, "
+        f"'c' to indicate it's correct: ").strip().lower()
+
+    # If the guess is correct, end the game
     if guess == 'c':
-        print('game over, your secrete number = '+ str(medium))
+        print(f'game over, your secrete number = {medium}!')
         state = False
+    # If the guess is too high, adjust the range accordingly
     elif guess == 'h':
         high = medium
-        medium = (high + low) // 2
+    # If the guess is too low, adjust the range accordingly
     elif guess == 'l':
         low = medium
-        medium = (high + low) // 2
+    # If user enters an invalid input, ask again
     else:
-        print('i didn\' understand your input.')
+        print("invalid input, please enter 'h', 'l', 'c'.")
